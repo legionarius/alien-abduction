f986417326afaf403048be206459da13b34b7876
Octogene <ooctogene@gmail.com>
2021-03-02 08:14:08 +0100

    feat: functional jump
    - Added temp tileset to test

--- src/Player.cpp
+++ src/Player.cpp
@@ -20,31 +20,38 @@ void Player::_input(const Ref<InputEvent> event) {
 	}
 }
 
-void Player::_process(const real_t delta) {
-	if (is_on_floor()) {
-	}
-	if (apply_impulse) {
-		Vector2 direction = get_local_mouse_position().normalized();
-		move_and_slide(
-				Vector2(direction.x * 10000.0 * std::abs(impulse), direction.y * std::abs(impulse) * 5000.0),
-				get_local_mouse_position());
-		apply_impulse = false;
-		impulse = 0.0;
-		impulse_time = 0.0;
-	}
-
+void Player::_physics_process(const real_t delta) {
 	if (generate_impulse) {
 		impulse_time += delta;
 		impulse = Math::sin(impulse_time + delta);
 		bubbleControl->_set_oscillation(impulse);
 	}
+
+	if (apply_impulse) {
+		direction = get_local_mouse_position().normalized();
+		motion.x = direction.x * (_jump_force.x * std::abs(impulse));
+		motion.y = direction.y * std::abs(impulse) * _jump_force.y;
+		apply_impulse = false;
+		impulse = 0.0;
+		impulse_time = 0.0;
+	}
+
+	motion.y += _gravity;
+	motion.x -= motion.x * _inertia * std::abs(direction.x) * delta;
+
+	move_and_slide(motion, floor);
 }
 
 void Player::_init() {
 	impulse = 0.0;
 	impulse_time = 0.0;
+	motion = Vector2(0, 0);
+	direction = Vector2(0, 0);
 	apply_impulse = false;
 	generate_impulse = false;
+	_gravity = 30;
+	_inertia = 2.0;
+	_jump_force = Vector2(600.0, 1000.0);
 }
 
 void Player::_ready() {
@@ -54,6 +61,9 @@ void Player::_ready() {
 void Player::_register_methods() {
 	register_method("_init", &Player::_init);
 	register_method("_ready", &Player::_ready);
-	register_method("_process", &Player::_process);
+	register_method("_physics_process", &Player::_physics_process);
 	register_method("_input", &Player::_input);
+	register_property("gravity", &Player::_gravity, 30.f);
+	register_property("inertia", &Player::_inertia, 2.f);
+	register_property("jump_force", &Player::_jump_force, Vector2(600.0, 1000.0));
 }
--- src/Player.h
+++ src/Player.h
@@ -11,6 +11,7 @@
 #include <InputEventKey.hpp>
 #include <InputEventMouseButton.hpp>
 #include <KinematicBody2D.hpp>
+#include <KinematicCollision2D.hpp>
 #include <Math.hpp>
 #include <Ref.hpp>
 
@@ -24,13 +25,19 @@ class Player : public KinematicBody2D {
 	real_t impulse;
 	bool generate_impulse;
 	bool apply_impulse;
+	Vector2 motion;
+	Vector2 direction;
+	float _gravity;
+	float _inertia;
+	Vector2 _jump_force;
+	const Vector2 floor = Vector2(0, -1);
 
 public:
 	static void _register_methods();
 	void _init();
 	void _ready();
 	void _input(const Ref<InputEvent> event);
-	void _process(const real_t delta);
+	void _physics_process(const real_t delta);
 };
 
 } // namespace godot
