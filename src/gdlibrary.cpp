45b816f22a9e85d1a476c8ecb06f3dc9d18476d9
Romain M <romain.mard@gmail.com>
2021-03-04 04:44:41 +0100

    feat: apply format

--- src/gdlibrary.cpp
+++ src/gdlibrary.cpp
@@ -11,8 +11,8 @@
 #include "MainScene.h"
 #include "Player.h"
 #include "TitleScreen.h"
-#include "UILevel.h"
 #include "TransitionScreen.h"
+#include "UILevel.h"
 
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
 	godot::Godot::gdnative_init(o);
